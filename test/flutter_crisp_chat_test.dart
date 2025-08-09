aac774297c213229f0c958f933553996847b51f3
Md. Al-Amin <amin15-1951@diu.edu.bd>
2025-08-10 00:07:41 +0600

    feat: Add tests for pushSessionEvent
    This commit introduces tests for the `pushSessionEvent` method in the `FlutterCrispChat` class.
    
    The tests verify that `pushSessionEvent` correctly calls the underlying platform method with the provided event name and color.
    
    The `MockFlutterCrispChatPlatform` has been updated to include a `setSessionAttributesArgs` property to capture the arguments passed to `pushSessionEvent` for verification in the tests.

--- test/flutter_crisp_chat_test.dart
+++ test/flutter_crisp_chat_test.dart
@@ -43,15 +43,25 @@ class MockFlutterCrispChatPlatform
 
   Map<String, dynamic>? setSessionSegmentsArgs;
   @override
-  void setSessionSegments(
-      {required List<String> segments, bool overwrite = false}) {
+  void setSessionSegments({
+    required List<String> segments,
+    bool overwrite = false,
+  }) {
     setSessionSegmentsArgs = {'segments': segments, 'overwrite': overwrite};
+  }
+
+  Map<String, dynamic>? setSessionAttributesArgs;
+  @override
+  Future<void> pushSessionEvent({
+    required String name,
+    SessionEventColor color = SessionEventColor.blue,
+  }) async {
+    setSessionAttributesArgs = {'name': name, 'color': color};
   }
 }
 
 void main() {
-  final FlutterCrispChatPlatform initialPlatform =
-      FlutterCrispChatPlatform.instance;
+  final initialPlatform = FlutterCrispChatPlatform.instance;
 
   CrispConfig config = CrispConfig(websiteID: "YOUR_WEBSITE_KEY");
 
@@ -76,23 +86,25 @@ void main() {
 
   group('setSessionString', () {
     test('calls platform method with correct arguments', () {
-      MockFlutterCrispChatPlatform fakePlatform = MockFlutterCrispChatPlatform();
+      MockFlutterCrispChatPlatform fakePlatform =
+          MockFlutterCrispChatPlatform();
       FlutterCrispChatPlatform.instance = fakePlatform;
 
       FlutterCrispChat.setSessionString(key: 'testKey', value: 'testValue');
-      expect(fakePlatform.setSessionStringArgs, equals({'key': 'testKey', 'value': 'testValue'}));
+      expect(fakePlatform.setSessionStringArgs,
+          equals({'key': 'testKey', 'value': 'testValue'}));
     });
 
     test('throws ArgumentError for empty key', () {
       expect(
-            () => FlutterCrispChat.setSessionString(key: '', value: 'testValue'),
+        () => FlutterCrispChat.setSessionString(key: '', value: 'testValue'),
         throwsA(isA<ArgumentError>()),
       );
     });
 
     test('throws ArgumentError for empty value', () {
       expect(
-            () => FlutterCrispChat.setSessionString(key: 'testKey', value: ''),
+        () => FlutterCrispChat.setSessionString(key: 'testKey', value: ''),
         throwsA(isA<ArgumentError>()),
       );
     });
@@ -100,60 +112,105 @@ void main() {
 
   group('setSessionInt', () {
     test('calls platform method with correct arguments', () {
-      MockFlutterCrispChatPlatform fakePlatform = MockFlutterCrispChatPlatform();
+      MockFlutterCrispChatPlatform fakePlatform =
+          MockFlutterCrispChatPlatform();
       FlutterCrispChatPlatform.instance = fakePlatform;
 
       FlutterCrispChat.setSessionInt(key: 'testKey', value: 123);
-      expect(fakePlatform.setSessionIntArgs, equals({'key': 'testKey', 'value': 123}));
+      expect(fakePlatform.setSessionIntArgs,
+          equals({'key': 'testKey', 'value': 123}));
     });
 
     test('throws ArgumentError for empty key', () {
       expect(
-            () => FlutterCrispChat.setSessionInt(key: '', value: 123),
+        () => FlutterCrispChat.setSessionInt(key: '', value: 123),
         throwsA(isA<ArgumentError>()),
       );
     });
   });
 
   group('getSessionIdentifier', () {
-    test('returns session ID from platform', () async {
-      MockFlutterCrispChatPlatform fakePlatform = MockFlutterCrispChatPlatform();
-      fakePlatform.mockSessionId = 'testSessionId';
-      FlutterCrispChatPlatform.instance = fakePlatform;
+    test(
+      'returns session ID from platform',
+      () async {
+        final fakePlatform = MockFlutterCrispChatPlatform();
+        fakePlatform.mockSessionId = 'testSessionId';
+        FlutterCrispChatPlatform.instance = fakePlatform;
 
-      final sessionId = await FlutterCrispChat.getSessionIdentifier();
-      expect(sessionId, 'testSessionId');
-    });
+        final sessionId = await FlutterCrispChat.getSessionIdentifier();
+        expect(sessionId, 'testSessionId');
+      },
+    );
 
-    test('returns null if platform throws error', () async {
-      MockFlutterCrispChatPlatform fakePlatform = MockFlutterCrispChatPlatform();
-      fakePlatform.getSessionIdentifierShouldThrowError = true;
-      FlutterCrispChatPlatform.instance = fakePlatform;
+    test(
+      'returns null if platform throws error',
+      () async {
+        final fakePlatform = MockFlutterCrispChatPlatform();
+        fakePlatform.getSessionIdentifierShouldThrowError = true;
+        FlutterCrispChatPlatform.instance = fakePlatform;
 
-      final sessionId = await FlutterCrispChat.getSessionIdentifier();
-      expect(sessionId, isNull);
-    });
+        final sessionId = await FlutterCrispChat.getSessionIdentifier();
+        expect(sessionId, isNull);
+      },
+    );
   });
 
   group('setSessionSegments', () {
-    test('calls platform method with correct arguments', () {
-      MockFlutterCrispChatPlatform fakePlatform = MockFlutterCrispChatPlatform();
-      FlutterCrispChatPlatform.instance = fakePlatform;
+    test(
+      'calls platform method with correct arguments',
+      () {
+        final fakePlatform = MockFlutterCrispChatPlatform();
+        FlutterCrispChatPlatform.instance = fakePlatform;
 
-      final segments = ['segment1', 'segment2'];
-      FlutterCrispChat.setSessionSegments(segments: segments, overwrite: true);
+        final segments = ['segment1', 'segment2'];
+        FlutterCrispChat.setSessionSegments(
+          segments: segments,
+          overwrite: true,
+        );
 
-      expect(fakePlatform.setSessionSegmentsArgs, equals({'segments': segments, 'overwrite': true}));
-    });
+        expect(
+          fakePlatform.setSessionSegmentsArgs,
+          equals({'segments': segments, 'overwrite': true}),
+        );
+      },
+    );
 
-    test('calls platform method with default overwrite value', () {
-      MockFlutterCrispChatPlatform fakePlatform = MockFlutterCrispChatPlatform();
-      FlutterCrispChatPlatform.instance = fakePlatform;
+    test(
+      'calls platform method with default overwrite value',
+      () {
+        final fakePlatform = MockFlutterCrispChatPlatform();
+        FlutterCrispChatPlatform.instance = fakePlatform;
 
-      final segments = ['segment_a', 'segment_b'];
-      FlutterCrispChat.setSessionSegments(segments: segments); // overwrite is false by default
+        final segments = ['segment_a', 'segment_b'];
+        FlutterCrispChat.setSessionSegments(
+          segments: segments,
+        ); // overwrite is false by default
 
-      expect(fakePlatform.setSessionSegmentsArgs, equals({'segments': segments, 'overwrite': false}));
-    });
+        expect(
+          fakePlatform.setSessionSegmentsArgs,
+          equals({'segments': segments, 'overwrite': false}),
+        );
+      },
+    );
+  });
+
+  group('pushSessionEvent', () {
+    test(
+      'calls platform method with correct arguments',
+      () async {
+        final fakePlatform = MockFlutterCrispChatPlatform();
+        FlutterCrispChatPlatform.instance = fakePlatform;
+
+        await FlutterCrispChat.pushSessionEvent(
+          name: 'testEvent',
+          color: SessionEventColor.red,
+        );
+
+        expect(
+          fakePlatform.setSessionAttributesArgs,
+          equals({'name': 'testEvent', 'color': SessionEventColor.red}),
+        );
+      },
+    );
   });
 }
